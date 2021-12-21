01d2b17a4e3c97729127a23befffb4c4999a602a
MONTCHO <shekmontcho@gmail.com>
2021-12-21 17:57:52 +0100

    v2

--- src/main/java/SMSBackend.java
+++ src/main/java/SMSBackend.java
@@ -1,6 +1,14 @@
 import com.twilio.Twilio;
+import com.twilio.http.TwilioRestClient;
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import com.twilio.twiml.Sms;
+import com.twilio.type.PhoneNumber;
 import spark.Spark;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 
@@ -17,19 +25,20 @@ public class SMSBackend {
         }
         Spark.port(port);
 
-        get("/home", (req, res) -> "Hello, World");
+        get("/welcome", (req, res) -> "Hello, World");
 
-        Twilio.init("AC52873427123d39917e51227e318e0106","07ff2b7a945809225c7f9110d045b6ad");
+       // Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN"));
+        Twilio.init("AC52873427123d39917e51227e318e0106","89c3946ce0d6e8fd454af8eddcda664a");
         post("/sms", (req, res) -> {
-            String body = req.queryParams("Body");
-            String to = "whatsapp:"+req.queryParams("To");
+            String body = "whatsapp:"+ req.queryParams("Body");
+            String to = "whatsapp:"+ req.queryParams("To");
             String from = "whatsapp:+14155238886";
 
             Message message = Message.creator(
-                            new com.twilio.type.PhoneNumber(to),
-                            new com.twilio.type.PhoneNumber(from),
-                            body)
-                    .create();
+                            new com.twilio.type.PhoneNumber("whatsapp:+22961877882"),
+                            new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                            "body").create();
+
 
             return message.getSid();
         });
