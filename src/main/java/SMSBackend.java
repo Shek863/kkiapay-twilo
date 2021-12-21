c385ccafa37563a1e10790265ebd86beca0e7bae
MONTCHO <shekmontcho@gmail.com>
2021-12-21 18:06:41 +0100

    v3

--- src/main/java/SMSBackend.java
+++ src/main/java/SMSBackend.java
@@ -25,20 +25,19 @@ public class SMSBackend {
         }
         Spark.port(port);
 
-        get("/welcome", (req, res) -> "Hello, World");
+        get("/home", (req, res) -> "Hello, World");
 
        // Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN"));
         Twilio.init("AC52873427123d39917e51227e318e0106","89c3946ce0d6e8fd454af8eddcda664a");
         post("/sms", (req, res) -> {
-            String body = "whatsapp:"+ req.queryParams("Body");
+            String body = req.queryParams("Body");
             String to = "whatsapp:"+ req.queryParams("To");
             String from = "whatsapp:+14155238886";
 
             Message message = Message.creator(
-                            new com.twilio.type.PhoneNumber("whatsapp:+22961877882"),
-                            new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-                            "body").create();
-
+                            new com.twilio.type.PhoneNumber(to),
+                            new com.twilio.type.PhoneNumber(from),
+                            body).create();
 
             return message.getSid();
         });
